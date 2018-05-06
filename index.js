234b20c7b113567bcebcf1d0b8d5d2b3aba63560
haykam821 <haykam821@users.noreply.github.com>
2018-05-06 15:08:17 -0400

    Beautify

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 module.exports = (action, logChannel, commandMessage, actedUpon, reason) => {
-    return new Promise(async resolve => {if (logChannel) {
-    logChannel.send("", {
-        embed: {
-            title: `Moderator Action: ${action}`,
-            fields: [
-                {
-                    name: "Moderator",
-                    value: commandMessage.author,
-                },
-                {
-                    name: "Channel Executed",
-                    value: commandMessage.channel,
-                },
-                {
-                    name: "User",
-                    value: actedUpon,
-                },
-                {
-                    name: "Reason",
-                    value: reason,
+    return new Promise(async resolve => {
+        if (logChannel) {
+            logChannel.send("", {
+                embed: {
+                    title: `Moderator Action: ${action}`,
+                    fields: [{
+                            name: "Moderator",
+                            value: commandMessage.author,
+                        },
+                        {
+                            name: "Channel Executed",
+                            value: commandMessage.channel,
+                        },
+                        {
+                            name: "User",
+                            value: actedUpon,
+                        },
+                        {
+                            name: "Reason",
+                            value: reason,
+                        }
+                    ],
+                    color: 0x7289DA,
+                    timestamp: new Date(),
                 }
-            ],
-            color: 0x7289DA,
-            timestamp: new Date(),
+            }).then(e => {
+                resolve(e);
+            });
         }
-    }).then(e=> {
-        resolve(e);
     });
-}});
 }
