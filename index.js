98d0d68c5705f823c17d482f79fe2437378de706
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-07-24 21:55:02 -0400

    Fallback if no reason provided

--- index.js
+++ index.js
@@ -4,22 +4,18 @@ module.exports = async (action, logChannel, commandMessage, actedUpon, reason) =
             embed: {
                 title: `Moderator Action: ${action}`,
                 fields: [{
-                        name: "Moderator",
-                        value: `${commandMessage.author.tag} / ${commandMessage.author}`,
-                    },
-                    {
-                        name: "Channel Executed",
-                        value: `#${commandMessage.channel.name} / ${commandMessage.channel}`,
-                    },
-                    {
-                        name: "User",
-                        value: `${actedUpon.user.tag} / ${actedUpon}`,
-                    },
-                    {
-                        name: "Reason",
-                        value: reason,
-                    }
-                ],
+                    name: "Moderator",
+                    value: `${commandMessage.author.tag} / ${commandMessage.author}`,
+                }, {
+                    name: "Channel Executed",
+                    value: `#${commandMessage.channel.name} / ${commandMessage.channel}`,
+                }, {
+                    name: "User",
+                    value: `${actedUpon.user.tag} / ${actedUpon}`,
+                }, {
+                    name: "Reason",
+                    value: reason || "*No reason provided.*",
+                }],
                 color: 0x7289DA,
                 timestamp: new Date(),
             }
