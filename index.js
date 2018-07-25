1921effdff27aa89aee2cebbdc3622b2934ae6e8
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-07-24 22:07:09 -0400

    Incomplete JSDoc

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * Logs an action in an embed.
+ * @param {string} action The action that is taking place.
+ * @param {} logChannel The channel to log to.
+ * @param {} commandMessage The message that initiated the action.
+ * @param {} actedUpon The user being acted upon.
+ * @param {string} reason The reason for this action.
+ */
 module.exports = async (action, logChannel, commandMessage, actedUpon, reason) => {
     const actedUser = actedUpon.user ? actedUpon.user : actedUpon;
     const actorUser = commandMessage.author;
