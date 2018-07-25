77dfa7114ff3a83e09f811941f33022ecf4ea2bd
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-07-24 21:46:23 -0400

    Convert promise to async function

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-module.exports = (action, logChannel, commandMessage, actedUpon, reason) => {
-    return new Promise(async resolve => {
-        if (logChannel) {
-            logChannel.send("", {
-                embed: {
-                    title: `Moderator Action: ${action}`,
-                    fields: [{
-                            name: "Moderator",
-                            value: `${commandMessage.author.tag} / ${commandMessage.author}`,
-                        },
-                        {
-                            name: "Channel Executed",
-                            value: `#${commandMessage.channel.name} / ${commandMessage.channel}`,
-                        },
-                        {
-                            name: "User",
-                            value: `${actedUpon.user.tag} / ${actedUpon}`,
-                        },
-                        {
-                            name: "Reason",
-                            value: reason,
-                        }
-                    ],
-                    color: 0x7289DA,
-                    timestamp: new Date(),
-                }
-            }).then(e => {
-                resolve(e);
-            });
-        }
-    });
+module.exports = async (action, logChannel, commandMessage, actedUpon, reason) => {
+    if (logChannel) {
+        return await logChannel.send("", {
+            embed: {
+                title: `Moderator Action: ${action}`,
+                fields: [{
+                        name: "Moderator",
+                        value: `${commandMessage.author.tag} / ${commandMessage.author}`,
+                    },
+                    {
+                        name: "Channel Executed",
+                        value: `#${commandMessage.channel.name} / ${commandMessage.channel}`,
+                    },
+                    {
+                        name: "User",
+                        value: `${actedUpon.user.tag} / ${actedUpon}`,
+                    },
+                    {
+                        name: "Reason",
+                        value: reason,
+                    }
+                ],
+                color: 0x7289DA,
+                timestamp: new Date(),
+            }
+        });
+    }
 }
