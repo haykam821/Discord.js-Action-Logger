b0a2282b821a77ffcd650d6df3afd36c5888931a
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-11-14 18:17:24 -0500

    Fix JSDoc syntax

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 /**
  * Logs an action in an embed.
  * @param {string} action The action that is taking place.
- * @param {} logChannel The channel to log to.
- * @param {} commandMessage The message that initiated the action.
- * @param {} actedUpon The user being acted upon.
+ * @param {djs.Channel} logChannel The channel to log to.
+ * @param {djs.Message} commandMessage The message that initiated the action.
+ * @param {(djs.User | djs.Member)} actedUpon The user being acted upon.
  * @param {string} reason The reason for this action.
+ * @returns {Promise<djs.Message>} A promise that resolves with the sent action log.
  */
-module.exports = (action, logChannel, commandMessage, actedUpon, reason) => {
+function logAction(action, logChannel, commandMessage, actedUpon, reason) {
 	const actedUser = actedUpon.user ? actedUpon.user : actedUpon;
 	const actorUser = commandMessage.author;
 
@@ -30,3 +31,4 @@ module.exports = (action, logChannel, commandMessage, actedUpon, reason) => {
 		});
 	}
 }
+module.exports = logAction;
